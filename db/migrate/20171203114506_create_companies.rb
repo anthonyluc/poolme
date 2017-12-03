c868bb2a840b64141496b9f6041a97f9a588247e
Anthony LUC <anthony.luc69@gmail.com>
2017-12-03 12:45:44 +0100

    add model Company

---
+++ db/migrate/20171203114506_create_companies.rb
@@ -0,0 +1,14 @@
+class CreateCompanies < ActiveRecord::Migration[5.1]
+  def change
+    create_table :companies do |t|
+      t.string :name
+      t.text :address
+      t.string :country
+      t.string :email
+      t.string :phone_number
+      t.string :website
+
+      t.timestamps
+    end
+  end
+end
