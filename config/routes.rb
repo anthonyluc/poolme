25ce02f8c46d2db8ceded21313a698dd0d9b6f4e
Anthony LUC <anthony.luc69@gmail.com>
2017-12-06 16:05:43 +0100

    refacto routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,11 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :companies do
-    resources :legal_representatives
+  resources :companies, only: [:show, :new, :create] do
+    resources :legal_representatives, only: [:show]
   end
-  resources :projects do
-    resources :roles do
-      resources :models, except: [:index, :destroy]
-    end
+  resources :projects, only: [:index, :show] do
+    resources :roles, only: [:index, :show]
   end
 
   namespace :cview do
