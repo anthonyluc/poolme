7a6a177a43b18baebc349820bbf3e5e15b627ea5
Anthony LUC <anthony.luc69@gmail.com>
2017-12-07 16:04:49 +0100

    profile + edit profile

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :profiles, only: [:show, :edit, :update]
   resources :companies, only: [:show, :new, :create] do
     resources :users, only: [:show]
   end
