961a0eed7aaed37d7d8c3d67671bd6722a1e9872
Marie Suard <msuard26@gmail.com>
2017-12-07 11:49:00 +0100

    project page works with project data and map

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171206205915) do
+ActiveRecord::Schema.define(version: 20171207103723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,6 +86,8 @@ ActiveRecord::Schema.define(version: 20171206205915) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "city"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["company_id"], name: "index_projects_on_company_id"
     t.index ["country"], name: "index_projects_on_country"
     t.index ["date_end"], name: "index_projects_on_date_end"
