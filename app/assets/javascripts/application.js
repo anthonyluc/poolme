d403dd45cf3b2866b0518ae967003104709bf41a
Olivier Grosset <olivier.grossetj@gmail.com>
2017-12-10 02:42:28 +0100

    a lot

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,4 +4,11 @@
 //= require jquery-fileupload/basic
 //= require cloudinary/jquery.cloudinary
 //= require attachinary
+//= require toastr
 //= require_tree .
+
+toastr.options = {
+
+ "positionClass": "toast-bottom-right"
+
+ }
