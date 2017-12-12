9e8d6d8e0981f5ad50540130426e3dabf73ec188
Marie Suard <msuard26@gmail.com>
2017-12-12 18:12:46 +0100

    Users are geocoded now

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171212160257) do
+ActiveRecord::Schema.define(version: 20171212163357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -224,6 +224,8 @@ ActiveRecord::Schema.define(version: 20171212160257) do
     t.datetime "token_expiry"
     t.string "photo"
     t.date "date_of_birth"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["company_id"], name: "index_users_on_company_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
