404c62c737d1befaf61552d8e474b8beb26935db
Anthony LUC <anthony.luc69@gmail.com>
2017-12-15 12:09:17 +0100

    ...

---
+++ app/assets/javascripts/refresh_auto.js
@@ -0,0 +1,43 @@
+// document.addEventListener('DOMContentLoaded', function() {
+// // function loadlink(){
+// //     // $('#chat').load('show.html.erb',function () {
+// //     //      $(this).unwrap();
+// //     // });
+// //     $('#chat').load('show.html.erb').hide();
+
+
+// // }
+
+// // loadlink(); // This will run on page load
+// // setInterval(function(){
+// //     loadlink() // this will run after every 5 seconds
+// // }, 5000);
+
+// // setTimeout(function(){
+// //    window.location.reload(1);
+// // }, 5000);
+
+// // var ut = document.querySelector(".counter");
+
+// //   ut.addEventListener("click", (event) => {
+// //     if (!confirm("Are you sure? You will send a message.")) {
+// //       event.preventDefault();
+// //     }
+// //   });
+
+//   console.log("haha");
+//   $(".counter a").click(function(event){
+
+
+
+//     console.log("hey");
+//     res = confirm("Are you sure? You will send a message.")
+//     console.log("res");
+//     console.log(res);
+//     if (!res) {
+//       console.log("yoyo");
+//       event.preventDefault();
+//       event.stopPropagation();
+//     }
+//     });
+//  });
