2d65f512e37133b748168fdb5a16c77aba13acb8
Anthony LUC <anthony.luc69@gmail.com>
2017-12-15 14:51:17 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(first_name: "toto", last_name: "TOTO", email: "toto@gmail.com", username: "toto", password: "castors", gender: "m")
-User.create(first_name: "titi", last_name: "TITI", email: "titi@gmail.com", username: "titi", password: "castors", gender: "m")
-User.create(first_name: "tata", last_name: "TATA", email: "tata@gmail.com", username: "tata", password: "castors", gender: "f")
+User.create(first_name: "Jeannine", last_name: "FREEMAN", email: "jeannine.freeman@gmail.com", username: "Jeannine", password: "castors", gender: "female", ethnicity: "european", hair_color: "black", haircut: "long", corpulence: "regular")
+User.create(first_name: "Brenda", last_name: "FRANCIS", email: "brenda.francis@gmail.com", username: "Brenda", password: "castors", gender: "female", ethnicity: "european", hair_color: "brown", haircut: "mid-length", corpulence: "regular")
+User.create(first_name: "Lucie", last_name: "BURNS", email: "lucie.burns@gmail.com", username: "Lucie", password: "castors", gender: "female", ethnicity: "european", hair_color: "blond", haircut: "long", corpulence: "regular")
+User.create(first_name: "Jean-Bernard", last_name: "MONTGOMERY", email: "jean-bernard.montgomery@gmail.com", username: "Jean-Bernard", password: "castors", gender: "male", ethnicity: "asian", hair_color: "white", haircut: "short", corpulence: "regular")
+User.create(first_name: "Daniel", last_name: "REYNOLDS", email: "daniel.reynorlds@gmail.com", username: "Daniel", password: "castors", gender: "male", ethnicity: "african", hair_color: "black", haircut: "short", corpulence: "regular")
+User.create(first_name: "Jocelyne", last_name: "RILEY", email: "jocelyne.riley@gmail.com", username: "Jocelyne", password: "castors", gender: "female", ethnicity: "european", hair_color: "red", haircut: "long", corpulence: "regular")
+User.create(first_name: "Johnny", last_name: "Holiday", email: "johnny.holiday@gmail.com", username: "Johnny", password: "castors", gender: "male", ethnicity: "european", hair_color: "brown", haircut: "short", corpulence: "regular")
